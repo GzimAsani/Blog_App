648cf63c57e0c51df3ac698d9214aa784ecedaf0
Gzim <gzimasanni@gmail.com>
2020-11-16 16:29:41 +0100

    Add the edit button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,4 +25,15 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
+
 end
