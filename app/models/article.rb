d012a91dc27a29564eed7f8050688148cd0dd2bd
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-17 10:13:08 +0100

    create interface for tagging_list

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,14 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+
+  end
+
 end
